b105be1fc604ae217a7ae15d124978fbf5f8ffd3
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-11-03 16:57:47 +0700

    tkinter sample

---
+++ OOP Exercise/oop_in_tkinter.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+root = Tk()
+
+label1 = Label(root, text="Label Satu")
+label2 = Label(root, text="Label Dua")
+button1 = Button(root, text="Tombol Satu")
+
+label1.pack()
+label2.pack()
+button1.pack()
+
+root.mainloop()
